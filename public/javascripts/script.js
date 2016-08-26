de2f516589aa340706afe7f1e8c578cb0b1fc6d7
Shirish Dali <designsbydali@gmail.com>
2016-08-26 12:12:10 -0500

    completed it

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,29 +1,44 @@
 $(document).ready(function() {
-  $('#magic-btn').on('click', function() {
-    var api = 'http://api.icndb.com/jokes/random';
-    $.ajax({
-        url: api
-      })
-      .done(function(data) {
-        // console.log(data);
-        $('#jokes').text(data.value.joke)
-
-      })
+  randomJoke();
+  $('#magic-btn').on('click', function(e) {
+    e.preventDefault()
+    randomJoke();
   })
-
-  $('#tweet-btn').on('click', function () {
-    // console.log('okay');
-    var api = 'http://api.icndb.com/jokes/random';
-    $.ajax({
-        url: api
-      }).done(function(data){
-        // console.log('happy');
-        $(this).attr("href", 'https://twitter.com/intent/tweet?text=' + data.value.joke);
-        console.log('neah');
-      })
+  $('#tweet-btn').on('click', function(e) {
+    e.preventDefault()
+    postTweet()
   })
 
 });
+
+function randomJoke() {
+  var api = 'http://api.icndb.com/jokes/random';
+  $.ajax({
+      url: api
+    })
+    .done(function(data) {
+      // console.log(data);
+      $('#jokes').text(data.value.joke)
+    })
+}
+
+function postTweet() {
+  var api = '/tweets';
+  $.ajax({
+    url: api,
+    method: 'POST',
+    data: {
+      joke: $('#jokes').text()
+    }
+  }).done(function(data) {
+    console.log(data);
+    randomJoke();
+  })
+}
+
+
+
+
 
 // $(document).ready(function() {
 //   $.getJSON(url, getQuote, 'jsonp');
